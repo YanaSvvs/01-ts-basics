cd977832508ea2a519c1a8d4fa8a05b7a1c1feaa
Yasmin <jana.afonina@gmail.com>
2025-07-17 12:17:49 +0300

    add task-7

---
+++ src/task-7.ts
@@ -0,0 +1,18 @@
+function getMessage(): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello from TS");
+    }, 1000);
+  });
+}
+
+getMessage().then(result => {
+  console.log(result);
+});
+
+async function processMessage() {
+  const message = await getMessage();
+  console.log(`Received: ${message.toUpperCase()}`); 
+}
+
+processMessage();
